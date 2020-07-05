7559e4c3f160554fa31fbdd61702ce4088c923a8
Cankat Örüm <52240938+JohnCut@users.noreply.github.com>
2020-07-05 22:16:11 +0300

    fetching data and crud
    - fetching todo items from jsonplaceholder using axios
    - crud operations with axios

--- src/App.js
+++ src/App.js
@@ -5,28 +5,18 @@ import Todos from './components/comps/Todos'
 import AddTodo from './components/comps/AddTodo'
 import About from './components/pages/About'
 import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   // state of the App component, includes a 'todos' array
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Cook dinner',
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: 'Do chores',
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: 'Read 100 pages',
-        completed: false
-      }
-    ]
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({ todos: res.data }))
   }
 
   // checks if the id matches the current state and changed 'completed' state to opposite value
@@ -44,18 +34,18 @@ class App extends Component {
   // checks if the id matches the current state and deletes(hides through filterring) it
   delTodo = (id) => {
     // ... is the spread operator
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+    axios.delete('http://jsonplaceholder.typicode.com/todos/${id}')
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+      )
   }
 
+  // adds todo item to the server and ui later
   addTodo = (title) => {
-    const newTodo = {
-      // it gives todo item a random id (using uuid package)
-      id: uuidv4(),
+    axios.post('http://jsonplaceholder.typicode.com/todos', {
       // (title: title) if name and value is the same, just write it once
       title,
       completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo] })
+    }).then(res => this.setState({ todos: [...this.state.todos, res.data] }))
   }
 
   render() {
